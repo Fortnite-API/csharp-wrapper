f9931807b36ed7436d9be1c137b27d88011e25a3
Not Officer <marlon.john007@gmail.com>
2019-11-19 14:21:15 +0100

    added 'Giftable' and 'Refundable'

--- src/Fortnite-API/Objects/BrShopEntry.cs
+++ src/Fortnite-API/Objects/BrShopEntry.cs
@@ -9,6 +9,8 @@ namespace Fortnite_API.Objects
 		[J("regularPrice")] public int RegularPrice { get; private set; }
 		[J("finalPrice")] public int FinalPrice { get; private set; }
 		[J("isBundle")] public bool IsBundle { get; private set; }
+		[J("giftable")] public bool Giftable { get; private set; }
+		[J("refundable")] public bool Refundable { get; private set; }
 		[J("panel")] public int Panel { get; private set; }
 		[J("sortPriority")] public int SortPriority { get; private set; }
 		[J("banner")] public string Banner { get; private set; }
