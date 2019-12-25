29b1c86b9247f0eacc9be2c8283a7860a2b030e2
Not Officer <marlon.john007@gmail.com>
2019-12-25 17:33:59 +0100

    added IsSpecial property

--- src/Fortnite-API/Objects/BrShopEntry.cs
+++ src/Fortnite-API/Objects/BrShopEntry.cs
@@ -9,6 +9,7 @@ namespace Fortnite_API.Objects
 		[J("regularPrice")] public int RegularPrice { get; private set; }
 		[J("finalPrice")] public int FinalPrice { get; private set; }
 		[J("isBundle")] public bool IsBundle { get; private set; }
+		[J("isSpecial")] public bool IsSpecial { get; private set; }
 		[J("giftable")] public bool Giftable { get; private set; }
 		[J("refundable")] public bool Refundable { get; private set; }
 		[J("panel")] public int Panel { get; private set; }
